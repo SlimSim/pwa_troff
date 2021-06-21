3a090d61e180b2bcfb89249ad4e38b7494be0191
Slim Sim <slimsimapps@gmail.com>
2021-06-21 16:29:41 +0200

    improving error logging with async

--- file.js
+++ file.js
@@ -26,12 +26,12 @@ $(function () {
 				if( nr <= 0 ) {
 					throw response;
 				} else {
-					return getTroffDataHelper( troffDataId, fileName, nr-1 );
+					return await getTroffDataHelper( troffDataId, fileName, nr-1 );
 				}
 			}
 			return response.payload;
 		})
-		.fail(function(xhr, status, error) {
+		.fail(async function(xhr, status, error) {
 				console.error( "fetchAndSaveResponseHelper, catch! url = " + url + ", nr = " + nr );
 				console.error( xhr );
 				console.error( status );
@@ -40,7 +40,7 @@ $(function () {
 				if( nr <= 0 ) {
 					throw error;
 				} else {
-					return getTroffDataHelper( troffDataId, fileName, nr-1 );
+					return await getTroffDataHelper( troffDataId, fileName, nr-1 );
 				}
 		});
 	};
@@ -50,25 +50,25 @@ $(function () {
 		console.log( "fetchAndSaveResponseHelper -> " + nr);
 		const url = environment.getDownloadFileEndpoint( fileId );
 		return fetch( url )
-			.then( (response) => {
+			.then( async (response) => {
 				if( !response.ok ) {
 					console.error( "fetchAndSaveResponseHelper, response is NOT ok, url = " + url + ", nr = " + nr );
 					console.error( response );
 					if( nr <= 0 ) {
 						throw response;
 					} else {
-						return fetchAndSaveResponseHelper( troffDataId, fileName, nr-1 );
+						return await fetchAndSaveResponseHelper( troffDataId, fileName, nr-1 );
 					}
 				}
 				return fileHandler.saveResponse( response, songKey );
 			})
-			.catch(function( e ) {
+			.catch(async function( e ) {
 				console.error( "fetchAndSaveResponseHelper, catch! url = " + url + ", nr = " + nr );
 				console.error( e );
 				if( nr <= 0 ) {
 					throw e;
 				} else {
-					return fetchAndSaveResponseHelper( troffDataId, fileName, nr-1 );
+					return await fetchAndSaveResponseHelper( troffDataId, fileName, nr-1 );
 				}
 			});
 	};
@@ -89,11 +89,11 @@ $(function () {
 
 	backendService.getTroffData = async function( troffDataId, fileName ) {
 		console.log( "backendService.getTroffData -> ");
-		return getTroffDataHelper( troffDataId, fileName, 3 );
+		return await getTroffDataHelper( troffDataId, fileName, 3 );
 	};
 
 	fileHandler.fetchAndSaveResponse = async function( fileId, songKey ) {
-		return fetchAndSaveResponseHelper( fileId, songKey, 3 );
+		return await fetchAndSaveResponseHelper( fileId, songKey, 3 );
 	};
 
 /*
