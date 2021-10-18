cd692c1888f43803e6e1454c9563f7f9de816476
Slim Sim <slimsimapps@gmail.com>
2021-10-18 11:38:28 +0200

    moving color-style before main style

--- service-worker.js
+++ service-worker.js
@@ -25,7 +25,7 @@
 var newAppCaches = [
 	{
 		name: 'core',
-		version: "1.6.17",
+		version: "1.6.18",
 		urls: [
 			"/",
 			"/index.html",
@@ -38,7 +38,7 @@ var newAppCaches = [
 	},
 	{
 		name: 'style-assets',
-		version: "1.3.22",
+		version: "1.3.23",
 		urls: [
 			"/stylesheets/style.css",
 			"/stylesheets/col1.css",
