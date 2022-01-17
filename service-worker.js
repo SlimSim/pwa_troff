3a51b8a8c732dd5ba7f10a447f8aea698d4ae151
Slim Sim <slimsimapps@gmail.com>
2022-01-17 16:28:34 +0100

    updating service worker

--- service-worker.js
+++ service-worker.js
@@ -25,7 +25,7 @@
 var newAppCaches = [
 	{
 		name: 'core',
-		version: "1.6.26",
+		version: "1.6.27",
 		urls: [
 			"/",
 			"/index.html",
@@ -85,7 +85,7 @@ var newAppCaches = [
 	},
 	{
 		name: 'internal-assets',
-		version: "1.2.20",
+		version: "1.2.21",
 		urls: [
 			"/assets/internal/common.js",
 			"/assets/internal/cookie_consent.js",
