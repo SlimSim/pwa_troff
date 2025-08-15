83532b6dd7f6e8ef6f3987b914914c487ca80dee
SlimSim <slimsimapps@gmail.com>
2025-08-15 08:38:44 +0200

    fix: potential bugs in sendMail

--- assets/internal/common.js
+++ assets/internal/common.js
@@ -15,37 +15,41 @@
 	along with Troff. If not, see <http://www.gnu.org/licenses/>.
 */
 
-(function(){
+(function () {
+  document.addEventListener("DOMContentLoaded", function (event) {
+    var shareButtons = document.querySelectorAll(".shareClass");
+    for (var i = 0; i < shareButtons.length; i++) {
+      shareButtons[i].addEventListener("click", sendMail);
+    }
+    function sendMail() {
+      console.log("sendMail 2 ->");
+      window.IO?.blurHack();
+      var subject = "Troff is a great music player for practicing";
+      var body =
+        "Hello\r\n\r\n" +
+        "I found this great app that is perfect for practicing dancing or " +
+        "instruments to songs. " +
+        "It let you loop a part of a song, slow it down " +
+        "and create markers on the song timeline.\r\n" +
+        "It even supports movies!\r\n\r\n" +
+        "It is free to use and download here:\r\n" +
+        "https://troff.app/\r\n\r\n" +
+        "Best regards!";
+      var link =
+        "mailto:?subject=" + subject + "&body=" + encodeURIComponent(body);
+      window.open(link);
+    }
 
-document.addEventListener("DOMContentLoaded", function(event){
-	var shareButtons = document.querySelectorAll(".shareClass");
-	for (var i = 0; i < shareButtons.length; i++){
-		shareButtons[i].addEventListener('click', sendMail);
-	}
-	function sendMail(){
-		console.log("sendMail 2 ->");
-		window.IO?.blurHack();
-		var subject = "Troff is a great music player for practicing";
-		var body = "Hello\n\n"
-			+ "I found this great app that is perfect for practicing dancing or "
-			+ "instruments to songs. "
-			+ "It let you loop a part of a song, slow it down "
-			+ "and create markers on the song timeline.\n"
-			+ "It even supports movies!\n\n"
-			+ "It is free to use and download here:\n"
-			+ "https://troff.app/\n\n"
-			+ "Best regards!";
-		var link = "mailto:?&subject="+subject+"&body=" + escape(body);
-		window.open(link);
-	}
+    let toggleNavigation = document.querySelectorAll(".toggle-navigation");
 
-	let toggleNavigation = document.querySelectorAll(".toggle-navigation");
-
-	toggleNavigation.forEach( function( item, index ) {
-		item.addEventListener( "click", function(){
-  		document.getElementById("navigation").classList.toggle( "d-none" );
-  	});
-	});
-
-});
+    toggleNavigation.forEach(function (item, index) {
+      item.addEventListener("click", function () {
+        const navigation = document.getElementById("navigation");
+        if (!navigation) {
+          return;
+        }
+        navigation.classList.toggle("d-none");
+      });
+    });
+  });
 })();
