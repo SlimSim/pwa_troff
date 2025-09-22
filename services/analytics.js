51693a4d86f8a9b4c7d9709f68e0dda232941b6f
SlimSim <slimsimapps@gmail.com>
2025-09-22 16:49:23 +0200

    chore: removing the old JS files...

--- services/analytics.js
+++
@@ -1,6 +0,0 @@
-function gtag() {
-  // log.d("gtag -> arguments:", arguments);
-  // TODO: should perhaps gather statistics in the future :)
-}
-
-export { gtag };
--- utils/browserEnv.js
+++
@@ -1,5 +0,0 @@
-export const treatSafariDifferent = false;
-
-export const isSafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
-export const isIphone = navigator.userAgent.indexOf('iPhone') !== -1;
-export const isIpad = navigator.userAgent.indexOf('iPad') !== -1;
